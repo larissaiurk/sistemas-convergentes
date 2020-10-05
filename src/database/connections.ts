a5aa947f0ab3a960b7ac8b6e83bde41d7e7b756b
Larissa <larissa.iurk@gmail.com>
2020-10-05 18:47:07 -0300

    fix procfile

--- src/database/connections.ts
+++ src/database/connections.ts
@@ -4,6 +4,7 @@ import path from 'path';
 const connections = knex({
   client: 'pg',
   connection: {
+    host: 'ec2-23-23-242-234.compute-1.amazonaws.com',
     database: 'dcd8ggi4do0uhp',
     user: 'kghtsrmubezfgq',
     password: '1db916bd5ef346ad5543edf81cd0c2414733729edf8c8ef05261839a89881c68'
