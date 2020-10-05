5c4b7fcab0a6cf5285b6a81ed68cdbfea04e74eb
Larissa <larissa.iurk@gmail.com>
2020-10-05 18:43:29 -0300

    fix procfile

--- knexfile.ts
+++ knexfile.ts
@@ -3,9 +3,9 @@ import path from 'path';
 module.exports = {
   client: 'pg',
   connection: {
-    database: 'sistemasconvergentes',
-    user: 'postgres',
-    password: 'password'
+    database: 'dcd8ggi4do0uhp',
+    user: 'kghtsrmubezfgq',
+    password: '1db916bd5ef346ad5543edf81cd0c2414733729edf8c8ef05261839a89881c68'
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
--- src/database/connections.ts
+++ src/database/connections.ts
@@ -4,9 +4,9 @@ import path from 'path';
 const connections = knex({
   client: 'pg',
   connection: {
-    database: 'sistemasconvergentes',
-    user: 'postgres',
-    password: 'password'
+    database: 'dcd8ggi4do0uhp',
+    user: 'kghtsrmubezfgq',
+    password: '1db916bd5ef346ad5543edf81cd0c2414733729edf8c8ef05261839a89881c68'
   },
   useNullAsDefault: true,
 });
